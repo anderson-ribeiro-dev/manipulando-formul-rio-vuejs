b1b8302d4459004b6cf9cb76e91162cb03a2e918
anderson silva ribeiro <anderson_silvaribeiro@hotmail.com>
2020-03-18 21:18:55 -0300

    submetendo formulário com form e div usando prevent

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 	<div id="app">
 		<h1>Registrar Reclamação</h1>
 		<div class="conteudo">
-			<form class="painel">
+			<div class="painel" v-if="!enviado">
 				<div class="cabecalho">Formulário</div>
 				<Rotulo nome="E-mail">
 					<!-- <input type="text" v-model="usuario.email"> -->
@@ -51,9 +51,9 @@
 					<Escolha v-model="escolha" />
 				</Rotulo>
 				<hr>
-				<button>Enviar</button>
-			</form>
-			<div class="painel">
+				<button @click.prevent="enviar">Enviar</button>
+			</div>
+			<div class="painel" v-else>
 				<div class="cabecalho">Resultado</div>
 				<Rotulo nome="E-mail">
 					<span>{{ usuario.email }}</span>
@@ -109,6 +109,11 @@ export default {
 			return typeof this.prioridade
 		}
 	},
+	methods: {
+		enviar() {
+			this.enviado = true
+		}
+	},
 	data() {
 		return {
 			// email: '',
@@ -132,9 +137,11 @@ export default {
 				idade: 25,
 			},
 			temp: 'teste',
-			escolha: true
+			escolha: true,
+			enviado: false
 		}
 	},
+
 	created() {
 		//two way data bind
 		// setTimeout(() => {
@@ -182,7 +189,7 @@ body {
 	font-size: 1.4rem;
 }
 
-#app form button {
+.painel button {
 	float: right;
 	margin: 10px 0px;
 	padding: 10px 20px;
